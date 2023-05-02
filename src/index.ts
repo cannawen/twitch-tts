58ab5ab1b4e67e6b612e9efab9ee2cdf7ce3a6d9
Canna Wen <cannawen@gmail.com>
2023-05-02 14:16:02 -0400

    Add TTS for hard coded twitch channel

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import tmi from "tmi.js";
+import path from "path";
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const messages: string[] = [];
+
+const client = new tmi.Client({
+  channels: ["humblekorean"],
+});
+
+client.connect();
+
+client.on("message", (channel, tags, message, self) => {
+  messages.push(`${tags["display-name"]} says ${message}`);
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).sendFile(path.join(__dirname, "../resources/index.html"));
+});
+
+app.get("/poll", (req, res) => {
+  const nextMessage = messages.shift();
+  res.status(200).json({ nextMessage: nextMessage });
 });
 
 app.listen(port, () => {
